7c3eaf9ed65f0feb534872cadc53634f17c4cbad
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-05-02 14:31:29 -0400

    fleshed out post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,3 +1,58 @@
-export default function Post(){
-  return <h1>All Posts Page!</h1>
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
+
+export default function Post() {
+  const [postData, setPost] = useState("");
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main className="min-h-screen p-12 main-skills">
+      <section className="container mx-auto">
+        <h1 className="skillsh1 text-5xl flex justify-center cursive">My Skills</h1>
+        <h2 className="skillsh2 text-large flex justify-center mb-12">
+          Welcome to my Technical Skills Page
+        </h2>
+        <div className="skills-div grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span className="skills-span block h-64 relative rounded shadow leading-snug bg-white border-green-400 key={index}">
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className="w-full h-full rounded-r object-cover absolute"
+                    />
+                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                        {post.title}
+                      </h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
 }
