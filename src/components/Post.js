e425456810cf2d1b9e360e950fdc00ea616e2c87
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-05-02 17:09:59 -0400

    removed post link

--- src/components/Post.js
+++ src/components/Post.js
@@ -35,20 +35,18 @@ export default function Post() {
           {postData &&
             postData.map((post, index) => (
               <article>
-                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                  <span className="skills-span block h-64 relative rounded shadow leading-snug bg-white border-green-400 key={index}">
+                  <span className="skills-span block h-64 relative rounded shadow leading-snug bg-white key={index}">
                     <img
                       src={post.mainImage.asset.url}
                       alt={post.mainImage.alt}
-                      className="w-full h-full rounded-r object-cover absolute"
+                      className="w-full h-full rounded-r object-contain absolute"
                     />
                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                      <h3 className="skills-title rounded">
                         {post.title}
                       </h3>
                     </span>
                   </span>
-                </Link>
               </article>
             ))}
         </div>
