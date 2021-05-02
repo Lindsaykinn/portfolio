d0fd523b98a6b6670be9a0a8c86c802a19ae00f6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-05-02 14:31:51 -0400

    fleshed out home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,12 @@
-export default function Home(){
-  return <h1>Home Page!</h1>
+import image from "../bg-img.png";
+
+export default function Home() {
+  return (
+    <main>
+      <img src={image} alt="Future" className="absolute object-cover w-full h-full" />
+      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+        <h1 className='text-6xl cursive leading-none lg:leading-snug home-name'>Hi. I'm Lindsay.</h1>
+      </section>
+    </main>
+  );
 }
