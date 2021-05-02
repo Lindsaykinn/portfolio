e687c0eed0ee7b53eeabd8e59e3fc0c88b6b49a7
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-05-02 17:40:28 -0400

    updated client.js

--- src/client.js
+++ src/client.js
@@ -3,5 +3,8 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
   projectId: 'ddyqjo6p',
-  dataset: 'production'
+  dataset: 'production',
+  apiVersion: '2021-03-25',
+  token: 'sanity-auth-token',
+  useCdn: false,
 })
